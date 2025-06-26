f50e0b535cdbd27439215a628c8f6d97064aeaca
Whady <martinez.whady@gmail.com>
2025-06-25 22:46:14 -0400

    fix de showmodal luego de cerra showdialog

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -833,12 +833,184 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
                 const SizedBox(height: 30),
                 FloatingActionButton(
-                  onPressed: () {
+                  onPressed: () async {
                     if (analysisStyle.isEmpty && analysisSelected == false) {
-                      showDialog(
+                      await showDialog(
                         context: context,
                         builder: (context) {
                           return SelectAnalysisStyle();
+                        },
+                      );
+
+                      await Future.delayed(Duration(milliseconds: 300));
+
+                      showModalBottomSheet(context: context,
+                        isScrollControlled: true,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.vertical(
+                            top: Radius.circular(12),
+                          ),
+                        ),
+                        backgroundColor:
+                        btnProvider.isButtonEnabled
+                            ? Colors.grey.shade900
+                            : Colors.white,
+                        builder: (BuildContext context) {
+                          return Padding(
+                            padding: EdgeInsets.only(
+                              left: 16.0,
+                              right: 16.0,
+                              top: 16.0,
+                              bottom:
+                              MediaQuery.of(context)
+                                  .viewInsets
+                                  .bottom, // Ajusta el espacio según el teclado
+                            ),
+                            child: SingleChildScrollView(
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    '¿Qué soñaste hoy?',
+                                    style: AppTextStyle.subtitleStyle(
+                                      btnProvider.isButtonEnabled
+                                          ? Colors.white
+                                          : Colors.grey.shade700,
+                                    ),
+                                  ),
+                                  const SizedBox(height: 16),
+                                  TextField(
+                                    textCapitalization: TextCapitalization.sentences,
+                                    style: TextStyle(
+                                      fontFamily: 'roboto',
+                                      color:
+                                      btnProvider.isButtonEnabled
+                                          ? Colors.white
+                                          : Colors.grey.shade700,
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                                    controller: _dreamController,
+                                    decoration: InputDecoration(
+                                      hintText: 'Escribe tu sueño...',
+                                      hintStyle: TextStyle(
+                                        color:
+                                        btnProvider.isButtonEnabled
+                                            ? Colors.white70
+                                            : Colors.grey.shade500,
+                                      ),
+                                      border: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(8),
+                                      ),
+                                    ),
+                                    maxLines: 5,
+                                  ),
+                                  const SizedBox(height: 10),
+                                  Row(
+                                    children: [
+                                      Icon(Iconsax.lock_1, color: Colors.white38, size: 15),
+                                      const SizedBox(width: 5,),
+                                      Text('Tus sueños se encuentran a salvo.', style: RobotoTextStyle.small2TextStyle(Colors.white38))
+                                    ],
+                                  ),
+                                  const SizedBox(height: 6),
+                                  SizedBox(
+                                    height: 70,
+                                    width: double.infinity,
+                                    child: ElevatedButton(
+                                      onPressed: () async {
+                                        FocusScope.of(context).unfocus();
+                                        if (_dreamController.text.trim().isEmpty) {
+                                          return;
+                                        }
+                                        try {
+                                          setState(() {
+                                            isLoading = true;
+                                          });
+                                          print(analysisStyleProvider.analysisStyle);
+                                          final title = await GeminiService()
+                                              .generateTitle(
+                                            _dreamController.text,
+                                            apiKey,
+                                            analysisStyle == '' ? analysisStyleProvider.analysisStyle: analysisStyle,
+                                          );
+                                          FirebaseService().saveDream(
+                                            context,
+                                            _dreamController,
+                                            _selectedDate,
+                                            title,
+                                            analysisStyle == '' ? analysisStyleProvider.analysisStyle: analysisStyle,
+                                          );
+                                        } catch (e) {
+                                          ScaffoldMessenger.of(context).showSnackBar(
+                                            SnackBar(
+                                              content: Text(
+                                                'Error, intente de nuevo',
+                                              ),
+                                            ),
+                                          );
+                                        } finally {
+                                          setState(() {
+                                            isLoading = false;
+                                          });
+                                          _dreamController.clear();
+                                          Navigator.pop(context);
+                                        }
+                                      },
+                                      style: ElevatedButton.styleFrom(
+                                        padding: const EdgeInsets.symmetric(
+                                          vertical: 12,
+                                        ),
+                                        shape: RoundedRectangleBorder(
+                                          borderRadius: BorderRadius.circular(8),
+                                        ),
+                                        backgroundColor: Colors.transparent,
+                                        shadowColor: Colors.transparent,
+                                      ),
+                                      child: Ink(
+                                        decoration: BoxDecoration(
+                                          gradient: LinearGradient(
+                                            colors: [
+                                              Colors.purple.shade400,
+                                              Colors.purple.shade600,
+                                              Colors.indigo.shade400,
+                                            ],
+                                            begin: Alignment.topLeft,
+                                            end: Alignment.bottomRight,
+                                          ),
+                                          borderRadius: BorderRadius.circular(8),
+                                        ),
+                                        child: Container(
+                                          alignment: Alignment.center,
+                                          child:
+                                          isLoading
+                                              ? SizedBox(
+                                            width: 25,
+                                            height: 25,
+                                            child: CircularProgressIndicator(
+                                              strokeWidth: 4,
+                                              valueColor:
+                                              AlwaysStoppedAnimation<
+                                                  Color
+                                              >(Colors.white),
+                                            ),
+                                          )
+                                              : Text(
+                                            'Guardar',
+                                            style: TextStyle(
+                                              fontFamily: 'roboto',
+                                              fontWeight: FontWeight.bold,
+                                              color: Colors.white,
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          );
                         },
                       );
                     } else if (analysisStyle.isNotEmpty || analysisSelected == true){
@@ -1010,7 +1182,8 @@ class _MyHomePageState extends State<MyHomePage> {
                             ),
                           );
                         },
-                      );}
+                      );
+                    }
                   },
                   child: Container(
                     height: 100,
